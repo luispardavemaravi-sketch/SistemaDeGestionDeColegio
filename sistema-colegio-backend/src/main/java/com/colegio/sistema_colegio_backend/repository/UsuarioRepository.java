7af8fe7e421138fd91e2b2ac6f97c31415a656c3
Luisangel <luispardavemaravi@gmail.com>
2026-08-19 14:12:14 -0500

    prueba

--- sistema-colegio-backend/src/main/java/com/colegio/sistema_colegio_backend/repository/UsuarioRepository.java
+++ sistema-colegio-backend/src/main/java/com/colegio/sistema_colegio_backend/repository/UsuarioRepository.java
@@ -4,10 +4,13 @@ import com.colegio.sistema_colegio_backend.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario,Integer> {
-    Usuario findByUsername(String username);
-    Usuario findByEmail(String email);
-    Usuario findByDni(String dni);
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    List<Usuario> findByUsername(String username);
+
+    List<Usuario> findByEmail(String email);
+
 
 }
